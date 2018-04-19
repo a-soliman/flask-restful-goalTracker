09bbe8a9fa11907535f7c96ac8d575107696da4c
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-19 14:38:23 -0700

    added a new resource refrence to get all Goals

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask;
 from flask_restful import Api, Resource, reqparse
 
-from resources.goal import Goal
+from resources.goal import Goal, Goals
 
 app = Flask(__name__)
 api = Api(app)
 
 
-api.add_resource(Goal,'/goals')
+api.add_resource(Goal,'/goals/<string:id>')
+api.add_resource(Goals, '/goals')
 
 if __name__ == '__main__':
     app.run(port=3333, debug=True)
