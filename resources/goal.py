630ead215b1609e6e7dec18f06dceb9fa49f40ff
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-19 14:39:31 -0700

    coded a post route and get list route

--- resources/goal.py
+++ resources/goal.py
@@ -23,8 +23,12 @@ class Goal(Resource):
         help="Deadline is required."
     )
 
-    def get(self):
-        pass
+    def get(self, id):
+        goal = GoalModel.find_by_id(id)
+
+        if goal is None:
+            return {'message': 'Not found'}, 404
+        return goal.json()
 
     def post(self):
         pass
@@ -35,7 +39,45 @@ class Goal(Resource):
     def delete(self):
         pass
 
+
 class Goals(Resource):
+    parser = reqparse.RequestParser()
+    
+    parser.add_argument('name',
+        type=str,
+        required=True,
+        help="Name is required."
+    )
+
+    parser.add_argument('type',
+        type=str,
+        required=True,
+        help="Type is required."
+    )
+
+    parser.add_argument('deadline',
+        type=str,
+        required=True,
+        help="Deadline is required."
+    )
+
+
     def get(self):
         # Returns a list of the saved Goals
-        pass
+        return {'Goals': [goal.json() for goal in GoalModel.get_goals()]}
+
+    def post(self):
+        # Addes a new Goal
+        data = Goals.parser.parse_args()
+
+        newGoal = GoalModel('someID', data['name'], data['type'], data['deadline'])
+        
+        try:
+            newGoal.save_to_db()
+        except:
+            return {'message': 'An Error accourd while saving.'}, 500
+        
+        return {'message': 'Added Goal'}, 201
+
+        
+        
