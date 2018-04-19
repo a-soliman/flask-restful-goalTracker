80f43c045e3a1ea8bf86b37d594507dff5310a34
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-19 14:38:52 -0700

    modified the model to save to db (in memory for now)

--- models/goal.py
+++ models/goal.py
@@ -1,3 +1,4 @@
+
 class GoalModel():
     def __init__(self, _id, name, type, deadline):
         self._id = _id
@@ -9,8 +10,21 @@ class GoalModel():
         return { "_id": self._id, "name": self.name, "type": self.type, "deadline": self.deadline }
 
     def save_to_db(self):
-        pass
+        goals.append(self)
+    
+    @classmethod
+    def get_goals(cls):
+        return goals
     
     @classmethod
     def find_by_id(cls, _id):
-        pass
+        for goal in goals:
+            if goal._id == _id:
+                return goal
+
+        return None
+
+goal1 = GoalModel('1', 'learn something new', 'Professional', 'tomorrow')
+goal2 = GoalModel('2', 'learn something new', 'Professional', 'tomorrow')
+goal3 = GoalModel('3', 'learn something new', 'Professional', 'tomorrow')
+goals = [ goal1, goal2, goal3 ]
