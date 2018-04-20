84af0eb1235298cec9fdf44ad69ba5b22ee767f6
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-20 16:17:21 -0700

    coded the resouces get, post, delete

--- resources/goal.py
+++ resources/goal.py
@@ -2,104 +2,71 @@ from flask_restful import Resource, reqparse
 
 from models.goal import GoalModel
 
+
 class Goal(Resource):
-    parser = reqparse.RequestParser()
+    def get(self, _id):
+        if not GoalModel.isValid_id(_id):
+            return {'message': 'Invalid ID'}, 400
+
+        goal = GoalModel.find_by_id(_id)
+        if goal:
+            return goal.json()
+        else:
+            return 'not found', 404
     
-    parser.add_argument('name',
-        type=str,
-        required=True,
-        help="Name is required."
-    )
+    def delete(self, _id):
+        if not GoalModel.isValid_id(_id):
+            return {'message': 'Invalid ID'}, 400
 
-    parser.add_argument('type',
-        type=str,
-        required=True,
-        help="Type is required."
-    )
-
-    parser.add_argument('deadline',
-        type=str,
-        required=True,
-        help="Deadline is required."
-    )
-
-    def get(self, id):
-        goal = GoalModel.find_by_id(id)
-
+        goal = GoalModel.find_by_id(_id)
         if goal is None:
-            return {'message': 'Not found'}, 404
-        return goal.json()
-    
-    def put(self, id):
-        goal = GoalModel.find_by_id(id)
-
-        if goal is None:
-            return {'message': 'Not found'}, 404
-        
-        data = Goal.parser.parse_args()
-        goal.name = data['name']
-        goal.type = data['type']
-        goal.deadline = data['deadline']
-
-        try:
-            goal.update_in_db(self)
-        except:
-            return {'message': 'An error occured while updating'}, 500
-        
-        return {'message': 'updated'}, 204
-
-    
-    def delete(self, id):
-        goal = GoalModel.find_by_id(id)
-        if goal is None:
-            return {'message': 'Goal not found'}, 400
+            return { 'messge': 'Goal was not found'}, 400
         
         try:
-            goal.remove_from_db()
+            goal.delete_from_db()
         except:
-            return {'message': 'An Error accoured'}, 500
+            return {'message': 'An Error Occured'}, 500
+
+        return {'message': 'Deleted'}
+    
+    def put(self, _id):
+        if not GoalModel.isValid_id(_id):
+            return {'message': 'Invalid ID'}, 400
         
-        return {'message': 'removed Goal'}
-
-
+        #To Be CONT....
+    
+    
 class Goals(Resource):
     parser = reqparse.RequestParser()
-    
+
     parser.add_argument('name',
-        type=str,
-        required=True,
-        help="Name is required."
+        type = str,
+        required = True,
+        help = 'name is required'
     )
 
     parser.add_argument('type',
-        type=str,
-        required=True,
-        help="Type is required."
+        type = str,
+        required = True,
+        help = "Type is required"
     )
 
     parser.add_argument('deadline',
-        type=str,
-        required=True,
-        help="Deadline is required."
+        type = str,
+        required = True,
+        help = "deadline is required"
     )
 
 
     def get(self):
-        # Returns a list of the saved Goals
-        return {'Goals': [goal.json() for goal in GoalModel.get_goals()]}
-
+        return { 'goals': [goal.json() for goal in GoalModel.get_all()]}
+    
     def post(self):
-        # Addes a new Goal
         data = Goals.parser.parse_args()
-
-        newGoal = GoalModel('someID', data['name'], data['type'], data['deadline'])
+        newGoal = GoalModel(_id= None,name=data['name'], _type=data['type'], deadline=data['deadline'])
         
         try:
             newGoal.save_to_db()
         except:
-            return {'message': 'An Error accourd while saving.'}, 500
-        
-        return {'message': 'Added Goal'}, 201
-
-        
-        
+            return { 'message': 'An error has occured.'}, 500
+        return {'message': 'Saved to DB'}, 201
