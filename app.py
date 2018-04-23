e3f2c9ddd17a6b4dbe22615e96e57d9d52196f03
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-23 14:41:31 -0700

    renamed dist to static

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_restful import Api
 from db import db
 
 
 from resources.goal import Goal, Goals
 
-app = Flask(__name__, static_url_path='/dist')
+app = Flask(__name__, static_url_path='')
 api = Api(app)
 
-@app.route('/')
-def root():
+@app.route('/<path:path>')
+def serve_page(path):
     return app.send_static_file('index.html')
 
+      
 api.add_resource(Goals, '/goals')
 api.add_resource(Goal, '/goals/<string:_id>')
 
