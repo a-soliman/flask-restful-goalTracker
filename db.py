2417bcb9b64191fafc2c15744dcc6e2f8a48f099
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-24 09:05:47 -0700

    added the fron end

--- db.py
+++
@@ -1,8 +0,0 @@
-'''
-This file contains mongodb uri
-'''
-from pymongo import MongoClient
-
-
-client = MongoClient('mongodb://ahmed_soliman:123456@ds247699.mlab.com:47699/goal-tracker')
-db = client['goal-tracker']
